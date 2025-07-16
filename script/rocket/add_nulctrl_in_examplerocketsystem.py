3f1c935d1294139bf7eb2f26ea4367b6ed92eff9
meng-cz <mengcz1126@gmail.com>
2025-07-16 02:37:55 +0000

    script for add nulctrl into rocket system

--- script/rocket/add_nulctrl_in_examplerocketsystem.py
+++ script/rocket/add_nulctrl_in_examplerocketsystem.py
@@ -1,13 +1,18 @@
 
 import sys 
 
-if len(sys.argv) < 4:
-    print("Usage: %s examplerocketsystem.v nulctrlmp.v output.v" % sys.argv[0])
+if len(sys.argv) < 5:
+    print("Usage: %s cpu_num ExampleRocketSystem.v NulctrlMpUart.v output.v" % sys.argv[0])
     exit(-1)
 
-rocket_path = sys.argv[1]
-nulctrl_path = sys.argv[2]
-output_path = sys.argv[3]
+cpu_num = int(sys.argv[1])
+rocket_path = sys.argv[2]
+nulctrl_path = sys.argv[3]
+output_path = sys.argv[4]
+
+if cpu_num < 2 or cpu_num > 16:
+    print("!!! 2 <= CPU_NUM <= 16 !!!")
+    exit(-1)
 
 rocket_lines = []
 nulctrl_lines = []
@@ -17,46 +22,124 @@ with open(rocket_path, "r") as ifile:
 with open(nulctrl_path, "r") as ifile:
     nulctrl_lines = ifile.readlines()
 
+for i in range(len(nulctrl_lines)):
+    nulctrl_lines[i] = nulctrl_lines[i].replace("Queue", "Nulqueue")
+
+nulctrl_port_list = [
+    ("output", "", "inited"),
+    ("output", "[1:0]", "priv"),
+    ("input", "", "ext_itr"),
+    ("input", "", "stop_fetch"),
+    ("input", "", "regacc_rd"),
+    ("input", "", "regacc_wt"),
+    ("input", "[4:0]", "regacc_idx"),
+    ("input", "[63:0]", "regacc_wdata"),
+    ("output", "[63:0]", "regacc_rdata"),
+    ("output", "", "regacc_busy"),
+    ("input", "", "inst64"),
+    ("input", "[31:0]", "inst64_raw"),
+    ("input", "", "inst64_nowait"),
+    ("output", "", "inst64_ready"),
+    ("input", "", "inst64_flush"),
+    ("output", "", "inst64_busy")
+]
+
 nulctrl_module_name = "NulCPUCtrlMPWithUart"
-nulctrl_port_prefix = "io_cpu_"
-nulctrl_tx_port = "io_txd"
-nulctrl_rx_port = "io_rxd"
+nulctrl_port_prefix = "io_cpu_%d_"
 
-# find nulctrl_ports
+module_port_prefix = "nul_"
+cpu_port_prefix = "io_nulctrl_"
 
-nul_input_ports = []
-nul_output_ports = []
-nul_cpu_num = 0
-
-l = 0
-for l in range(len(nulctrl_lines)):
-    if nulctrl_lines[l].startswith("module %s" % nulctrl_module_name):
-        l += 1
+def find_line_start_with(idx: int, s : str) -> int :
+    i = idx
+    while i < len(rocket_lines):
+        if not rocket_lines[i].startswith(s):
+            i += 1
+            continue
         break
-
-while not nulctrl_lines[l].startswith(")"):
-    words = nulctrl_lines[l].replace(",", "").replace("\n", "").split(" ")
-    words = [v for v in words if len(v)]
-    name = words[-1]
-    if not name.startswith(nulctrl_port_prefix):
-        l += 1
-        continue
-    name = name[len(nulctrl_port_prefix):]
-    idx = name.find("_")
-    cpu = int(name[:idx])
-    port = name[idx+1:]
-    if cpu == 0:
-        if words[0] == "input":
-            nul_input_ports.append(port)
-        else:
-            nul_output_ports.append(port)
-    nul_cpu_num = max(nul_cpu_num, cpu)
-    l += 1
-nul_cpu_num += 1
-
-print("NulCtrl has %d CPU Ports" % nul_cpu_num)
-print("Input Ports: %s" % ", ".join(nul_input_ports))
-print("Output Ports: %s" % ", ".join(nul_output_ports))
+    return i
 
 
+print("Find TOP module: ExampleRocketSystem")
+idx = find_line_start_with(0, "module ExampleRocketSystem")
+if idx == len(rocket_lines):
+    print("Module ExampleRocketSystem not found")
+    exit(-1)
 
+print("Insert RX/TX ports")
+rocket_lines.insert(idx+3, "  input         nul_rxd,\n")
+rocket_lines.insert(idx+3, "  output        nul_txd,\n")
+idx = find_line_start_with(idx, ");") + 1
+
+wire_name_prefix = "nul_%d_"
+
+print("Init wires")
+for cpu in range(cpu_num):
+    rocket_lines[idx:idx] = [str("  wire " + a[1] + " " + (wire_name_prefix % cpu) + a[2] + ";\n") for a in nulctrl_port_list]
+idx += cpu_num * len(nulctrl_port_list)
+
+print("Init NulCtrl sub-module")
+lines = [
+    "  %s nulctrl ( // Generated\n" % nulctrl_module_name,
+    "    .clock(clock)\n",
+    "   ,.reset(reset)\n",
+    "   ,.io_txd(nul_txd)\n",
+    "   ,.io_rxd(nul_rxd)\n"
+]
+for cpu in range(cpu_num):
+    lines += ([str("   ,." + (nulctrl_port_prefix % cpu) + a[2] + "(" + (wire_name_prefix % cpu) + a[2] + ")\n") for a in nulctrl_port_list])
+lines.append("  );\n")
+rocket_lines[idx:idx] = lines
+
+print("Connect sub-module to TilePRCIDomain")
+for cpu in range(cpu_num):
+    idx = find_line_start_with(idx, "  TilePRCIDomain")
+    if idx == len(rocket_lines):
+        print("Sub-module TilePRCIDomain(%d) not found" % cpu)
+        exit(-1)
+    idx += 1
+    rocket_lines[idx:idx] = [str("    ." + module_port_prefix + a[2] + "(" + (wire_name_prefix % cpu) + a[2] + ")\n") for a in nulctrl_port_list]
+
+
+print("Find module : TilePRCIDomain")
+idx = find_line_start_with(0, "module TilePRCIDomain")
+if idx == len(rocket_lines):
+    print("Module TilePRCIDomain not found")
+    exit(-1)
+
+print("Insert nul_ ports")
+idx += 1
+rocket_lines[idx:idx] = [("  %s %s %s%s,\n" % (a[0], a[1], module_port_prefix, a[2])) for a in nulctrl_port_list]
+
+print("Connect to RocketTile")
+idx = find_line_start_with(idx, "  RocketTile")
+if idx == len(rocket_lines):
+    print("Sub-module RocketTile not found")
+    exit(-1)
+idx += 3
+rocket_lines[idx:idx] = [("    .%s%s(%s%s),\n" %(module_port_prefix, a[2], module_port_prefix, a[2])) for a in nulctrl_port_list]
+
+
+print("Find module : RocketTile")
+idx = find_line_start_with(0, "module RocketTile")
+if idx == len(rocket_lines):
+    print("Module RocketTile not found")
+    exit(-1)
+
+print("Insert nul_ ports")
+idx += 1
+rocket_lines[idx:idx] = [("  %s %s %s%s,\n" % (a[0], a[1], module_port_prefix, a[2])) for a in nulctrl_port_list]
+
+print("Connect to Rocket")
+idx = find_line_start_with(idx, "  Rocket")
+if idx == len(rocket_lines):
+    print("Sub-module Rocket not found")
+    exit(-1)
+idx += 3
+rocket_lines[idx:idx] = [("    .%s%s(%s%s),\n" %(cpu_port_prefix, a[2], module_port_prefix, a[2])) for a in nulctrl_port_list]
+
+print("Generate verilog file ...")
+with open(output_path, "w") as ofile:
+    ofile.writelines(nulctrl_lines)
+    ofile.writelines(rocket_lines)
+print("Done")
